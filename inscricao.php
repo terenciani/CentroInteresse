ef84f62aad50a8b7d0fb77f27ff2f9c8b2dc6452
marcelo terenciani <marcelo.figterenciani@gmail.com>
2016-10-05 08:46:03 -0400

    5/10/2016

--- inscricao.php
+++ inscricao.php
@@ -1,12 +1,12 @@
 ﻿<?php include 'includes/conexao.php'; ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Inscrição do Centro Interesse</title>
-<link href="bootstrap-3.3.6/css/bootstrap.min.css" rel="stylesheet">
-<script src="bootstrap-3.3.6/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-<script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Inscrição do Centro Interesse</title>
+	<link href="bootstrap-3.3.6/css/bootstrap.min.css" rel="stylesheet">
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<script>
 	function SomenteNumero(e) {
 		var tecla = (window.event) ? event.keyCode : e.which;
 		if ((tecla > 47 && tecla < 58))
@@ -42,32 +42,39 @@
 </script>
 </head>
 <body>
-	<div id="container-um" class="thumbnail">
-		<div id="container-dois" class="thumbnail">
 			<h1>Inscrição do Centro Interesse</h1>
-		</div>
+			
+			
 		<form id="formulario" action="salvarinscricao.php"
 			onsubmit="return confirmou(this);" role="form" method="POST">
+			
+			
 			<div class="form-group">
 				<label for="txt_nome"><span
-					class="glyphicon glyphicon-text-background" aria-hidden="true"></span>
+					class="glyphicon glyphicon-user" aria-hidden="true"></span>
 					NOME:</label> <input type="text" class="form-control" id="txt_nome"
 					name="nome" onkeypress='return SomenteLetra(event)' required/>
 			</div>
+			
+			
 			<div class="form-group">
-				<label for="txt_codigo"><span class="glyphicon glyphicon-barcode"
+				<label for="txt_codigo"><span class="glyphicon glyphicon-list-alt"
 					aria-hidden="true"></span> CÓDIGO DO ALUNO:</label> <input
 					type="text" class="form-control" id="txt_codigo" name="meucodigo"
 					onkeypress='return SomenteNumero(event)' required/>
 			</div>
+			
+			
 			<div class="form-group">
 				<label for="txt_turma"><span class="glyphicon glyphicon-education"
 					aria-hidden="true"></span> TURMA:</label> <input type="text"
 					class="form-control" id="txt_turma" name="turma" required/>
 			</div>
+			
+			
 			<div class="form-group">
 				<label for="centrodeinsteresse-um"><span
-					class="glyphicon glyphicon-star" aria-hidden="true"></span> CENTRO
+					class="glyphicon glyphicon-pencil" aria-hidden="true"></span> CENTRO
 					DE INTERESSE 1:</label> <SELECT class="form-control"
 					ID="centrodeinsteresse-um" name="opcao1">
 					<OPTION VALUE="0">Insira...</option>
@@ -78,14 +85,13 @@
 							echo "<OPTION VALUE='" . $resultado [0] . "'>" . $resultado [1] . "</option>";
 						}
 						?>
-					
-
-
-				</SELECT>
+					</SELECT>
 			</div>
+			
+			
 			<div class="form-group">
 				<label for="centrodeinsteresse-dois"><span
-					class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+					class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 					CENTRO DE INTERESSE 2:</label> <SELECT class="form-control"
 					ID="centrodeinsteresse-dois" name="opcao2">
 					<OPTION VALUE="0">Insira...</option>
@@ -98,21 +104,26 @@
 					?>
 				</SELECT><br>
 			</div>
+			
+			
 			<h2>
 				<INPUT TYPE="checkbox" NAME="TERMODEDECLARACAO"
-					ID="TERMODEDECLARACAO" VALUE="IE3" />DECLARO ESTAR CIENTE, QUE NÃO
-				PODEREI TROCAR DE CENTRO DE INTERESSE DURANTE O SEMESTRE.<br>
+ID="TERMODEDECLARACAO" VALUE="IE3" />Declaro estar ciente, que não poderei trocar de centro de interesse entre o semestre.<br>
 			</h2>
+			
+			
 			<h2>
 				<button type="reset" class="btn btn-danger">
-					<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>CANCELAR
+					<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> CANCELAR
 				</button>
 				<button type="submit" class="btn btn-success">
-					<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>CONFIRMAR
+					<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> CONFIRMAR
 				</button>
 			</h2>
 
+
 		</form>
-	</div>
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="bootstrap-3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
